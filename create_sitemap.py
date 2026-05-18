1de295af8d41a6afb7f5a2516b1b98d5fdf50a9c
sergiokendrick12 <sergiokendrick12@gmail.com>
2026-05-18 14:08:25 +0200

    feat: add sitemap.xml for SEO

---
+++ create_sitemap.py
@@ -0,0 +1,14 @@
+﻿content = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://internationalfrenchacademy.org/</loc>
+    <lastmod>2026-05-18</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>
+"""
+f = open('public/sitemap.xml', 'w', encoding='utf-8')
+f.write(content)
+f.close()
+print('Done!')
